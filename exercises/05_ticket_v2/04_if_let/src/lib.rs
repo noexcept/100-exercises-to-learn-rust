b80fadbd5096b0386d18c8b05ea687b8300c413b
noexcept <zhaowenwei@outlook.com>
2025-04-14 22:10:10 +0800

    feat(exercies): day4 execrises

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,19 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        if let Shape::Circle { radius } = self {
+            *radius
+        } else {
+            panic!("Only `Circle` shapes have a radius")
+        }
+
+        /*
+        let radius = match self {
+            Shape::Circle { radius } => radius,
+            _ => panic!("Only `Circle` shapes have a radius"),
+        };
+        *radius
+         */
     }
 }
 
